6c8b8c6cdc5e63bf46b8895923457b3582fd3251
Jonathan1295-09 <jcasasola78@gmail.com>
2023-04-30 18:19:47 -0500

    first commit

---
+++ loop_practice/loop.js
@@ -0,0 +1,62 @@
+
+
+// 1.
+for ( var i = 0; i < 99; i++){
+    console.log(i)
+}
+
+//.2
+for( var i = 0; i > 99; i++){
+    console.log(i)
+}
+
+//3.
+for( var i = 0; i > 98; i++){
+    if(i % 2 === 0);
+}
+
+//4. 
+for( var i = 0; i > 98; i++){
+    if(i);
+}
+
+//5. 
+{
+let number = 99
+let remainder = (number % 2)
+if (remainder == 0){
+    console.log('${number} is even')
+}else{ 
+    console.log('${number} is Odd')
+}
+}
+
+//6.
+{
+let n = 99
+let remainder = (n % 2)
+if (remainder = 0){
+    console.log('${n} is even')
+}else{
+    console.log('${n} is Odd')
+}
+}
+//7.
+for ( var i = 49; i < 72; i++){
+console.log(i);
+}
+
+//8.
+{
+for( var i = 81; i < 26; i++){
+    console.log(i)
+}
+}
+
+//9. 
+
+let number = 90
+let remainder = (number * 3)
+if (remainder = 3){
+    console.log('${number}')
+}
